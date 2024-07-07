19158705fc099ab1bd0a9e7dae8876d3d48b3c7f
Denis <69577507+Desnn1ch@users.noreply.github.com>
2024-07-07 10:31:39 +0300

    Update data_class.py

--- internal/models/data_class.py
+++ internal/models/data_class.py
@@ -1,10 +1,12 @@
-from internal.models.request_model import TargetAudience, Point
+from pydantic import BaseModel
 from typing import List
 
-class RequestData:
-    def __init__(self, targetAudience: TargetAudience, points: List[Point]):
-        self.targetAudience = targetAudience
-        self.points = points
+class TargetAudience(BaseModel):
+    gender: str
+    ageFrom: int
+    ageTo: int
+    income: str
 
-    def __repr__(self):
-        return f"RequestData(targetAudience={self.targetAudience}, points={self.points})"
+class RequestData(BaseModel):
+    targetAudience: TargetAudience
+    tch: List[float]
