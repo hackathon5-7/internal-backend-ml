bd9226e50bf306a08e13f242052110fbb0313b52
Denis <69577507+Desnn1ch@users.noreply.github.com>
2024-07-07 10:33:38 +0300

    Update internal.py

--- internal/routers/internal.py
+++ internal/routers/internal.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, HTTPException
-from internal.models.request_model import RequestModel
 from internal.models.data_class import RequestData
 from internal.services.prediction_service import get_prediction_result
 
@@ -10,18 +9,12 @@ async def get_place(data: RequestModel):
     """
     Эндпоинт на получение оптимального места размещения рекламы
     """
-    # try:
-    #     request_data = RequestData(
-    #         targetAudience=request.targetAudience,
-    #         points=request.points
-    #     )
-        
-    #     prediction = get_prediction_result(request_data)
-
-    #     response = {"prediction": prediction}
-    #     return response
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    try:
+        value = get_prediction_result(data)
+        response = {"value": value}
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     return {}
 
 @router.get("/status/")
